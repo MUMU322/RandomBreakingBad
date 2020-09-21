8731238e86b74ece887743ed56cf509e8bc86fff
Antoine Barthes <antoine.barthes322@gmail.com>
2020-09-22 01:10:22 +0200

    BreakingBadApp V.01 simple random character

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # RandomBreakingBad
 App to get a random breaking bad character from public API. Made for a job inteview at SoShop
+
+## Features
+* show a random Breaking Bad character with a random button to get a new one.
+
+## Upgrade possible
+* Work the look and feel of the app
+* Add a detail button to get detailed informations
+* Search for a character to get informations
+* make a little quizz game with local storage of score.
+
+
+## Sources and dependencies
+* [okhttp3](https://square.github.io/okhttp/)
+* [Glide](https://github.com/bumptech/glide)
--- app/src/main/java/com/example/randombreakingbad/MainActivity.java
+++ app/src/main/java/com/example/randombreakingbad/MainActivity.java
@@ -3,12 +3,142 @@ package com.example.randombreakingbad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+
+import com.bumptech.glide.Glide;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Main activity manage our first page
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button bouton = findViewById(R.id.button); //Button to get a random character
+        //call randomCharacter a first time on launch
+        randomCharacter();
+
+        //Listener on the button to get a new random character
+        bouton.setOnClickListener(v -> randomCharacter());
+
+    }
+
+    /**
+     * Method to fetch a random character from the API and give it to the updateUI methode as a JSONObject
+     */
+    private void randomCharacter() {
+
+        //OkHttp3 request
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://www.breakingbadapi.com/api/character/random")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            /**
+             * Place to manipulate the result from the response.
+             * @param call
+             * @param response response of the request (with header and body...)
+             * @throws IOException
+             */
+            @Override
+            public void onResponse(@NotNull Call call, final Response response) throws IOException {
+                //we check that the response is sucessful, that she have code in 200..300
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                } else {
+                    String res = response.body().string();
+                    try {
+                        //first we create a JSONArray to read it because this is not a JSONObject from the start
+                        JSONArray arrayRes = new JSONArray(res);
+                        //from the JSONArray we create a JSONObject
+                        JSONObject objectRes = new JSONObject(arrayRes.getString(0));
+                        Log.i("2", arrayRes.toString());
+                        //update the UI with the character data
+                        updateUI(objectRes);
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * Method to update the character UI to the new recieved character
+     * @param objectRes a character informations
+     */
+    private void updateUI(JSONObject objectRes){
+        //We define some variables to access our views
+        final ImageView photo = findViewById(R.id.imageView); //Picture of the character
+        final TextView nom = findViewById(R.id.champNom); //Name of the character
+        final TextView surnom = findViewById(R.id.champSurnom); //Nickname of the character
+        final TextView metier = findViewById(R.id.champMetier); //Jobs of the character
+        final TextView saisons = findViewById(R.id.champSaisons); //Seasons he was on the show
+        //Necessary as glide.into() only run on main thread
+        runOnUiThread(() -> {
+            try {
+                //Load the image from the link given at the "img" URL into our image view
+                Glide.with(getApplicationContext())
+                        .load(objectRes.getString("img"))
+                        .into(photo);
+                //Load the name
+                nom.setText(objectRes.getString("name"));
+
+                //Load the nickname
+                surnom.setText(objectRes.getString("nickname"));
+
+                //Load the Jobs
+                //As the job are listed in a JSONArray within the occupation field we get each one in a loop to avoid undesirable character
+                JSONArray tabmetier = objectRes.getJSONArray("occupation");
+                String textMetier = "";
+                for (int i = 0; i < tabmetier.length(); i++) {
+                    //we only put comma if there is multiple jobs
+                    if (i > 0) {
+                        textMetier += ", " + tabmetier.getString(i);
+                    }
+                    else{
+                        textMetier += tabmetier.getString(i);
+                    }
+                }
+                metier.setText(textMetier);
+
+                //Load the Seasons
+                String textSeason = objectRes.getString("appearance");
+                //Another way to remove undesirable character ([] and ,) here with regex and replaceAll
+                textSeason = textSeason.replaceAll("[^0-9]", "  ");
+                saisons.setText(textSeason);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        });
     }
 }
+
